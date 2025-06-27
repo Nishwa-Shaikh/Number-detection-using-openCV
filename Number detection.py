db3c03d1d1aa3898953889c6e0e3ca83491b5e7c
Nishwa-Shaikh <nishwashaikh135@gmail.com>
2025-06-27 13:34:11 +0500

    Getting syntax error

--- Number detection.py
+++ Number detection.py
@@ -1,16 +1,23 @@
-import cv2 as cv
+'''import cv2 as cv
 import pytesseract as py
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+if not cap.isOpened():
+    print("Camera failed to open.")
+    exit()
+'''
+import cv2 as cv
+import tesseract as py
+capture = cv.VideoCapture(0) 
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    ret, frame = capture.read()
+    '''if not ret:
+        break'''
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     _, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
-    text = py.image_to_string(thresh, config='--psm 6 -c tessedit_char_whitelist=0123456789')
+    text = py.image_to_string(thresh, config='--psm.6 -c tessedit_char_whitelist=0123456789')
     cv.putText(frame, f"Detected: {text.strip()}", (10, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv.imshow('Number detection', frame)
     if cv.waitKey(20) & 0xFF == ord('q'):
         break
-cap.release()
+capture.release()
 cv.destroyAllWindows()
