f471d003cd44fe838f9b24a3f560e5bf81cce26a
Nishwa-Shaikh <nishwashaikh135@gmail.com>
2025-06-27 13:01:41 +0500

    Change camera settings

--- Number detection.py
+++ Number detection.py
@@ -3,14 +3,14 @@ import pytesseract as py
 cap = cv.VideoCapture(0)
 while True:
     ret, frame = cap.read()
-    if ret == False:
+    if not ret:
         break
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
-    text = py.image_to_string(thresh, config = '--psm 6 digits')
-    cv.putText(frame, f"Detected: {text.strip()}", (10, 50). cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv.imshow('Number detection')
-    if cv.waitKey(1)&0xFF == ord('q'):
+    _, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+    text = py.image_to_string(thresh, config='--psm 6 -c tessedit_char_whitelist=0123456789')
+    cv.putText(frame, f"Detected: {text.strip()}", (10, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv.imshow('Number detection', frame)
+    if cv.waitKey(20) & 0xFF == ord('q'):
         break
 cap.release()
 cv.destroyAllWindows()
